ea828b95eb0cbd477a93f6151ee761cc0c72c95e
LeoneCarmelo <carmeloleone@outlook.it>
2023-05-05 17:58:12 +0200

    Add style

--- index.php
+++ index.php
@@ -15,29 +15,29 @@ include 'functions.php';
     <!-- Bootstrap CSS v5.2.1 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <header>
-        <div class="container text-center">
+        <div class="container text-center py-4">
             <h1 class="fw-bold">Strong Password Generator</h1>
-            <h3>Genera una password sicura</h3>
+            <h3 class="text-white">Genera una password sicura</h3>
         </div>
     </header>
     <main>
         <div class="container">
-            <div class="output my-3 py-3 text-center">
+            <div class="output my-3 py-3 text-center w-75 mx-auto rounded-2">
             <?php     
                 genPassword($length,$dataLength,$data)    
                  ?>
             </div>
-            <form action="" class="w-75 mx-auto">
+            <form action="" class="w-75 mx-auto p-4 rounded-2">
                 <div class="cont_length d-flex justify-content-between">
                     <label for="length">Lunghezza password:</label>
                     <input type="text" name="length" id="length">
                 </div>
-                <div class="repetitions d-flex justify-content-between my-3 me-5">
+                <div class="repetitions d-flex justify-content-between my-3">
                     <label for="repetitions">Consenti ripetizioni di uno o più caratteri:</label>
                     <div class="cont_radio d-flex flex-column">
                         <div class="si d-flex">
@@ -50,7 +50,7 @@ include 'functions.php';
                         </div>
                     </div>
                 </div>
-                <div class="options d-flex flex-column align-items-end me-5">
+                <div class="options d-flex flex-column align-items-end">
                     <div class="option_1">
                         <input type="checkbox" name="lettere" id="lettere" class="me-1">
                         <label for="lettere" class="mx-2">Lettere</label>
